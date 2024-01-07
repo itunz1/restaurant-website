11f4b2f5e47161217b5d54c26060a31f8d7e75de
itunz1 <stx_jose@hotmail.com>
2024-01-07 11:38:39 -0400

    deals section

--- js/script.js
+++ js/script.js
@@ -9,3 +9,46 @@ menuBtn.addEventListener("click", function() {
 closeBtn.addEventListener("click", function() {
     menus.classList.remove("active");
 })
+
+
+// SwiperJS code
+
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 3,
+//     spaceBetween: 30,
+//     slidesPerGroup: 1,
+//     loop: true,
+//     loopFillGroupWithBlank: true,
+//     freeMode: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+//   });
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loopFillGroupWithBlank: true,
+    freeMode: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        968: {
+            slidesPerView: 3,
+        },
+    }
+  });
